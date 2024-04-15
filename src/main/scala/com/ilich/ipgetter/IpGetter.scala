6b029bcdf7ee2a342df9196f53c4cc30080e39e4
Yaroslav Ilich <yar.ilich@gmail.com>
2024-04-15 03:58:50 +0300

    Dirty version. Tests don't work with exponential backoff schedule

--- src/main/scala/com/ilich/ipgetter/IpGetter.scala
+++ src/main/scala/com/ilich/ipgetter/IpGetter.scala
@@ -3,30 +3,41 @@ package com.ilich.ipgetter
 import zio._
 import zio.http._
 import scala.util.matching.Regex
+import java.util.concurrent.TimeUnit
+import zio.Clock.currentTime
 
 object IpGetter extends ZIOAppDefault {
-  val numberPattern: Regex = """\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3}""".r
+  val NumberPattern: Regex = """\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3}""".r
+  val ExponentialTwice = Schedule.recurs(2) && Schedule.exponential(500.millis)
 
-  def tryParseIp(body: String): IO[NumberFormatException, String] = 
-    ZIO.getOrFailWith(new NumberFormatException(s"Failed to parse IP from response body: $body"))(numberPattern.findFirstIn(body))
+  def tryParseIp(body: String): IO[NumberFormatException, String] = {
+    lazy val exception = new NumberFormatException(s"Failed to parse IP from response body: $body")
+    ZIO.getOrFailWith(exception)(IpGetter.NumberPattern.findFirstIn(body))
+  }
 
-  val url = URL.decode("https://api.ipify.org/?format=json").toOption.get
+  def validateStatus(s: Status) = ZIO.whenCase(s){
+    case Status.Ok => ZIO.succeed(())
+    case _         => ZIO.fail(new IllegalStateException(s"HTTP response status is $s"))
+  }
 
-  val exponentialTwice = Schedule.exponential(1.second) && Schedule.recurs(2)
+  def requestAndParse(client: Client, urlString: String): ZIO[Scope, Throwable, String] = {
+    val url = URL.decode(urlString).toOption.get
+    for {
+      time   <- currentTime(TimeUnit.SECONDS)
+      _      <- Console.printLine(s"attempting request:$time")
+      res    <- client.url(url).get("/")
+      _      <- validateStatus(res.status)
+      data   <- res.body.asString
+      ip     <- tryParseIp(data) 
+    } yield ip
+  }
 
-  def requestAndParse(client: Client): ZIO[Scope, Throwable, String] = for {
-    res    <- client.url(url).get("/")
-    data   <- res.body.asString
-    ip     <- tryParseIp(data) 
-  } yield ip
-
-  val program = for {
+  def program(urlString: String = "https://api.ipify.org/?format=json") = for {
     client <- ZIO.service[Client]
-    ip     <- requestAndParse(client) retry exponentialTwice
-    _      <- Console.printLine(ip)
+    ip     <- requestAndParse(client, urlString) retry ExponentialTwice
+    _      <- Console.printLine(ip) // handle errors
   } yield ()
 
-  override val run = program.provide(Client.default, Scope.default)
-  // test error body, test unavailable service, test numberpattern matching
+  override val run = program().provide(Client.default, Scope.default)
 
 }
---
+++ src/test/scala/com/ilich/ipgetter/IpGetterSpec.scala
@@ -0,0 +1,71 @@
+package com.ilich.ipgetter
+
+import zio._
+import zio.http._
+import zio.http.netty.client.NettyClientDriver
+import zio.test._
+import zio.test.Assertion._
+
+import java.io.IOException
+import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.client.WireMock._
+import com.github.tomakehurst.wiremock.WireMockServer
+
+import IpGetter._
+
+
+object IpGetterSpec extends ZIOSpecDefault {
+  val wireMockServer = new WireMockServer()
+  // test error body
+  def spec = suite("IpGetterSpec")(
+    suite("tryParseIpSpec")(
+      test("tryParseIp correctly parses IP from response") {
+        for {
+          ip      <- tryParseIp("""{"ip": "192.168.0.19"}""")
+        } yield assertTrue(ip == "192.168.0.19")
+      },
+      test("tryParseIp fails with NumberFormatException if it cannot parse IP") {
+        assertZIO(tryParseIp("Hi").exit)(fails(isSubtype[NumberFormatException](anything)))
+      }
+    ),
+    suite("IpGetterRequestSpec")(
+      test("request fails after being retried three times when return status is 500 and the body is parseable") {
+        stubFor(get("/?format=json")
+          .willReturn(aResponse().withStatus(500).withBody("""{"ip":"176.100.1.212"}""")))
+
+        for {
+          client <- ZIO.service[Client]
+          result  <- requestAndParse(client, "http://127.0.0.1:8080/?format=json")
+            .retry(ExponentialTwice)
+            .orElse(ZIO.attempt(verify(3, getRequestedFor(urlEqualTo("/?format=json"))))).exit
+        } yield assertTrue(result.isSuccess)
+      },
+      test("request succeeds and ip is printed to the console") {
+        stubFor(get("/?format=json")
+          .willReturn(ok("""{"ip":"176.100.1.212"}""")))
+
+        for {
+          client <- ZIO.service[Client]
+          result  <- requestAndParse(client, "http://127.0.0.1:8080/?format=json")
+            .retry(ExponentialTwice)
+        } yield assertTrue(result == "176.100.1.212") 
+      }
+      /* ,
+      test("request fails again") {
+        for {
+          client <- ZIO.service[Client]
+          fiber  <- (client.url(URL.decode("http://127.0.0.1").toOption.get).get("/") retry IpGetter.ExponentialTwice).fork
+          _      <- TestClock.adjust(1.second)
+          _      <- TestClock.adjust(1.second)
+          result <- fiber.join.exit
+        } yield assertTrue(result.isFailure)
+      } */
+    ).provideShared(Client.default, Scope.default) 
+    @@ TestAspect.after(ZIO.succeed(WireMock.reset()))
+    @@ TestAspect.beforeAll(ZIO.succeed(wireMockServer.start()))
+    @@ TestAspect.afterAll(ZIO.succeed(wireMockServer.stop()))
+  )
+  
+  
+
+}
